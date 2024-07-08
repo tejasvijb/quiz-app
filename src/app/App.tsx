c98a04c896bb31e1fbe4d200603f25ac5c580afb
tejasvijb <jbtejasvi@gmail.com>
2024-07-09 01:35:01 +0530

    scroll-fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <main className='flex justify-center items-center align-center h-full'>
-      <section className='relative drop-shadow-lg	rounded-md w-[450px] sm:h-fit sm:min-h-[570px] max-sm:h-screen border overflow-hidden'>
-       
+      <section className='relative drop-shadow-lg	rounded-md w-[450px] sm:h-fit sm:min-h-[570px] max-sm:h-screen border overflow-y-auto'>
+
         <Outlet />
 
 
