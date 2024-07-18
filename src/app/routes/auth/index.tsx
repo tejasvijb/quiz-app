5d904f94daa62228e48e81ae501608376ea5d42a
tejasvijb <jbtejasvi@gmail.com>
2024-07-18 22:54:44 +0530

    change routes object

--- src/app/routes/auth/index.tsx
+++ src/app/routes/auth/index.tsx
@@ -12,11 +12,17 @@ export default function AuthLayout() {
   // )
 
   return (
-    <div className="relative h-full">
-      <div className="absolute inset-0 bg-[url('/background.png')] bg-red-200 bg-cover bg-center opacity-15"></div>
-      <div className="relative h-full z-10">
-        <Outlet />
-      </div>
-    </div>
+    <main className='flex justify-center items-center align-center h-full'>
+      <section className='relative 	rounded-md w-[450px] h-screen border '>
+        <div className="relative h-full">
+          <div className="absolute inset-0 bg-[url('/background.png')] bg-red-200 bg-cover bg-center opacity-15"></div>
+          <div className="relative h-full z-10">
+            <Outlet />
+          </div>
+        </div>
+      </section>
+    </main >
+
+
   )
 }
