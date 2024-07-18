1c16fadae91661ec4a2c56689e910529725a2b97
tejasvijb <jbtejasvi@gmail.com>
2024-07-19 01:09:33 +0530

    fix 101

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import Layout from './app/App.tsx'
 import './index.css'
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider,
 } from "react-router-dom";
 // import QuizDescription from './components/app/QuizDescription.tsx'
@@ -17,66 +15,66 @@ import Login from './app/routes/auth/login/index.tsx';
 import Register from './app/routes/auth/register/index.tsx';
 import ForgotPassword from './app/routes/auth/forgot-password/index.tsx';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "/auth",
-//         element: <AuthLayout />,
-//         children: [
-//           {
-//             path: "/auth/login",
-//             element: <Login />
-//           },
-//           {
-//             path: "/auth/register",
-//             element: <Register />
-//           },
-//           {
-//             path: "/auth/forgot-password",
-//             element: <ForgotPassword />
-//           }
-//         ]
-//       },
-//       {
-//         path: "/",
-//         element: <Home />,
-//       },
-//       {
-//         path: '/quiz/:quizId',
-//         element: <QuizDescription />,
-//       },
-//       {
-//         path: '/quiz/:quizId/question/:questionId',
-//         element: <Question />,
-//       },
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/auth/login",
+            element: <Login />
+          },
+          {
+            path: "/auth/register",
+            element: <Register />
+          },
+          {
+            path: "/auth/forgot-password",
+            element: <ForgotPassword />
+          }
+        ]
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: '/quiz/:quizId',
+        element: <QuizDescription />,
+      },
+      {
+        path: '/quiz/:quizId/question/:questionId',
+        element: <Question />,
+      },
 
-//     ]
-//   },
-//   {
-//     path: "/not-found",
-//     element: <div>Not found</div>
-//   }
-// ]);
+    ]
+  },
+  {
+    path: "/*",
+    element: <div>Not found</div>
+  }
+]);
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path={'/quiz/:quizId'} element={<QuizDescription />} />
-      <Route path={'/quiz/:quizId/question/:questionId'} element={<Question />} />
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route index element={<Home />} />
+//       <Route path={'/quiz/:quizId'} element={<QuizDescription />} />
+//       <Route path={'/quiz/:quizId/question/:questionId'} element={<Question />} />
 
-      <Route path="/auth" element={<AuthLayout />}>
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-      </Route>
-      {/* ... etc. */}
-    </Route>
-  )
-);
+//       <Route path="/auth" element={<AuthLayout />}>
+//         <Route path="/auth/login" element={<Login />} />
+//         <Route path="/auth/register" element={<Register />} />
+//         <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+//       </Route>
+//       {/* ... etc. */}
+//     </Route>
+//   )
+// );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
